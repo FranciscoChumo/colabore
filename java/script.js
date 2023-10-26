af0e2e62ba19cee876fa3a89d63fb4554cbe83a3
FranciscoChumo <frchd.1316334083@gmail.com>
2023-10-26 14:58:29 -0500

    rediriga a otra pagina2

--- java/script.js
+++ java/script.js
@@ -1,6 +1,11 @@
 function regist(){
-    debugger
-    let regis=document.getElementById("text")
-    if(regis.value=="email" ){
+    //debugger
+    let regis=document.getElementById("email").value;
+    var regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(regex.test(regis) ){
+        alert('Registered Successfully');
+        window.location.href="pagina2.html";
+}else{
+    alert('Invalid Email Address');
 }
 }
